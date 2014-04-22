011910e93e086923b95e1acbb2cd6772384ccd27
lemaivien <lemaivien@gmail.com>
2014-04-23 00:09:35 +0700

    remove polymophic from activitiesw

--- app/models/User.php
+++ app/models/User.php
@@ -52,10 +52,6 @@ class User extends SentryModel {
         ),
     );
 
-    public function activities() {
-        return $this->morphMany('Activity', 'author');
-    }
-
     public static function getDefaultPermission() {
         $sharedPermissions = array('home', 'error', 'logout');
         $catalogerPermissions = array('book.create', 'book.catalog', 'book.preview');
