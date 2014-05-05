137219c7088bf35e1ad80e5f334c782d1f2b6ca5
lemaivien <lemaivien@gmail.com>
2014-05-05 21:53:59 +0700

    hehe

--- app/controllers/ActivityController.php
+++ app/controllers/ActivityController.php
@@ -5,12 +5,6 @@ class ActivityController extends \BaseController {
     public function search() {
         $activities = Activity::search(Input::only('group', 'range'))
             ->paginate(Activity::PER_PAGE);
-//        return Response::json(array(
-//                'success' => true,
-//                'html' => View::make('activity._listing')
-//                    ->with('activities', $activities)
-//                    ->render()
-//        ));
         return View::make('activity._listing')
                 ->with('activities', $activities);
     }
