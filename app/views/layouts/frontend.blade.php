81fab8a7eb4a7ea203654b9d7978378907b668e8
lemaivien <lemaivien@gmail.com>
2014-06-11 21:09:50 +0700

    added text to footer

--- app/views/layouts/frontend.blade.php
+++ app/views/layouts/frontend.blade.php
@@ -20,14 +20,13 @@
             </div><!--end of center content-->
             <div class="margin-15"></div>
             <div class="footer">
-                <!--                <div class="left_footer text-italic text-muted">
-                                    Bản quyền thuộc về Thư Viện T52 - 2014
-                                </div>
-                                <div class="right_footer">
-                                    <a href="#">Trang chủ</a>
-                                    <a href="#">Giới thiệu</a>
-                                </div>-->
-                <img src="<?php echo asset('img/footer.png') ?>"/>
+                <div class="left_footer">
+                    Bản quyền thuộc về Thư Viện T52 - Năm {{date('Y')}}
+                </div>
+                <div class="right_footer">
+                    <a href="<?php echo route('fe.home')?>">Trang chủ</a>
+                    <a href="#">Giới thiệu</a>
+                </div>
             </div>
         </div>
         <script type="text/javascript" src="{{{asset('js/plugins/jquery-2.0.0.min.js')}}}"></script>
